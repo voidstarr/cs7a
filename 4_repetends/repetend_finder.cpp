23e61e7dc7929862fc24077f80ad897d4121236c
voidstarr <voidstarr@users.noreply.github.com>
2016-10-02 14:34:58 -0700

    First draft of repetend finder

--- 4_repetends/repetend_finder.cpp
+++ 4_repetends/repetend_finder.cpp
@@ -19,28 +19,74 @@ The digits of the repetend are
 //           http://pari.math.u-bordeaux.fr/
 //           http://seanelvidge.com/2011/12/recurring-decimals-something-on-the-repetend/
 
+#define QUOTIENT_LENGTH 200
+#define MAX_REPETEND_LENGTH 50
+
 #include <iostream>
 #include <vector>
-using namespace std ;
+
+using namespace std;
+
+void printQuotient(int);
+
+vector<short> decimalDigs;
+
 int main () {
-    int dividend, divisor, quotient, remainder;
-    vector<short> decimalDigs;
+    uint64_t dividend, divisor, quotient, remainder;
     cout << "\nEnter the dividend and divisor of the rational number "
          << " whose repetend we seek :\n";
     
     cin >> dividend >> divisor;
     
-    for (int i = 0; i < 200; i++) {
-        decimalDigs.push_back ( dividend / divisor );
+    for (uint64_t i = 0; i < QUOTIENT_LENGTH; i++) {
+        //decimalDigs.push_back(dividend / divisor);
         remainder = dividend % divisor;
         dividend = 10 * remainder;
-
+        decimalDigs.push_back(dividend / divisor);
     }
     
     cout << "\nHere are the digits of the quotient computed : ";
-    for (int i = 0; i < 200; i++)
+    printQuotient(QUOTIENT_LENGTH);
+    // Write code to detect the repetend and its length here
+/*    
+    uint64_t n = 1;
+
+    while (n < MAX_REPETEND_LENGTH) {
+        uint64_t i = 0;
+        while (decimalDigs[i] == decimalDigs[i + n]) {
+            i++;
+            if (i > MAX_REPETEND_LENGTH) {
+                cout << "Found repetend." << endl;
+                printQuotient(n);
+                return 0;
+            }
+        }
+        n++;
+    }
+
+    cout << "No repetend found." << endl;
+*/
+
+
+    // I find this for-loop solution the most elegant 
+    for (int n = 1; n < MAX_REPETEND_LENGTH; n++) {
+        for (int i = 0; decimalDigs[i] == decimalDigs[i + n]; i++) {
+            if (i > MAX_REPETEND_LENGTH) {
+                cout << "Found repetend." << endl;
+                printQuotient(n);
+                return 0;
+            }
+
+        }
+    }
+  
+    cout << "No repetend found." << endl;
+   
+    return 0;
+}
+
+void printQuotient(int digits) {
+    for (uint64_t i = 0; i < digits; i++)
         cout << decimalDigs[i];
     cout << endl;
-    // Write code to detect the repetend and its length here
-    return 0;
 }
