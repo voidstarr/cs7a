21820beba6d188909e9d100960fa57909e480787
voidstar <voidstarr@users.noreply.github.com>
2016-10-27 14:58:29 -0700

    Initial commit for assignment 5

---
+++ 5_15_puzzle/slider_puzzle.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+// Prototype descriptions (enter a prototype that fits.) 
+
+// display() shows it on the console.  For example, if 
+// board = {1, 2, 3, 4, 5, 16, 7, 8, 9, 10, 11, 12, 13, 14, 15, 6}
+// then display shows
+//  1  2  3  4  
+//  5     7  8
+//  9 10 11 12
+// 13 14 15  6 
+
+//shuffle() Use the \href{http://en.wikipedia.org/wiki/Fisher–Yates_shuffle}{Fisher-Yates} algorithm to shuffle.
+//After shuffling the board, a call to the function display will produce a 
+//shuffled board like this:
+/* 14  5  4 11
+   12  9  6  7
+      13  8 10
+   15  1  3  2 */
+
+// 
+//  1  2  3  4  
+//  5  6  7  8
+//  9 10 11 12
+// 13 14 15   
+
+
+
+class Board {
+    public:
+    	int size = 4; // The dimension of the square board
+    	vector<int> tiles;
+		void getMove(tiles);  // prompts the use to enter u, d, l, r 
+							  // (or some other scheme)  	
+    	void shuffle(tiles);
+		void display(tiles);
+		bool won(); //returns true if the game board is as below, otherwise false
+		Board(); //	constructor
+};
+
+void shuffle(&vector<?>)
+
+int main() 
+{
+// Initialize board with blank tiles in the lower right corner:
+    // create a Board, b
+    //game loop
+	while(!b.won())
+	{	
+		// construct a board with size*size tiles
+		getMove(board, size); // function calls – size is const int,
+		display(board, size); // board is the address in memory of the
+	}                          // first element of the array, board[]
+}
+
+//define display()
+
+void shuffle(&vector<?> v) {
+    for (int i = v.size(); i > 0; i--){
+        
+    }
+}
+
+// define won()
+
+// define getMove()
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # because I'm tired of typing this over and over
-clang++-3.5 -std=c++14 -o $2 $1
+clang++-3.8 -std=c++14 -o $2 $1
